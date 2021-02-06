528b06b95c633c17373c0d159e27806213e83930
김서영 <ksy990628@gmail.com>
2021-02-06 18:55:39 +0900

    210206
    REACT - 여태 수강한 과목 선택(coursecheck부분)

--- demo2/board-back/src/main/java/com/board/back/model/Majors.java
+++ demo2/board-back/src/main/java/com/board/back/model/Majors.java
@@ -1,7 +1,14 @@
 package com.board.back.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import javax.persistence.*;
 
 @Entity
 @Table(name = "Majors")
