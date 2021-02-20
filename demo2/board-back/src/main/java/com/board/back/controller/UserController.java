6840ca340763d587f7abb64ca2ff3c02af7de3a8
betty214 <50865204+betty214@users.noreply.github.com>
2021-02-20 22:47:55 +0900

    controller 수정

--- demo2/board-back/src/main/java/com/board/back/controller/UserController.java
+++ demo2/board-back/src/main/java/com/board/back/controller/UserController.java
@@ -1,4 +1,5 @@
 package com.board.back.controller;
+
 import com.board.back.model.User;
 import com.board.back.model.User2;
 import com.board.back.service.LoginService;
@@ -6,53 +7,52 @@ import com.board.back.service.UserService;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
-
 public class UserController {
     private LoginService loginService;
     private UserService userService;
-
     @Autowired
     public UserController(UserService userService, LoginService loginService) {
-        this.userService = userService;
+        this.userService=userService;
         this.loginService = loginService;
     }
-//login
+    // create board
+
     @PostMapping("/login")
-    public void loginUser(@RequestBody User2 user) {
+    public void loginUser(@RequestBody User user) {
         System.out.println("@PostMapping(\"/login\")");
-//        System.out.println(user.toString());
-//        System.out.println(user.getId());
-        List<User2> us = loginService.findAll(user.getId());
+        System.out.println(user.toString());
     }
-
-//    @GetMapping(value = "/login2")
-//    public void list(Model model) {
-//        List<User2> us = loginService.findById();
-//        System.out.println(us);
-//    }
-
-// create board
+    @GetMapping(value = "/login2")
+    public void list(Model model) {
+        List<User2> us= loginService.getAllUser();
+        System.out.println(us);
+    }
     @PostMapping("/user")
     public User createUser(@RequestBody User user) {
-//        System.out.println("@PostMapping(\"/user\")");
-//        System.out.println(user.toString());
+        System.out.println("@PostMapping(\"/user\")");
+        System.out.println(user.toString());
         return userService.createUser(user);
     }
     /*
-     * @PostMapping("/test") public void createTest(@RequestBody String testLine) {
-     * UserInput.insert_query(testLine); }
-     *
-     * @GetMapping("/user/{user_no}") public ResponseEntity<User> getUserByNo(
-     *
-     * @PathVariable Integer user_no) { return userService.getUser(user_no); }
-     */
+    @PostMapping("/test")
+    public void createTest(@RequestBody String testLine) {
+        UserInput.insert_query(testLine);
+    }
+        @GetMapping("/user/{user_no}")
+    public ResponseEntity<User> getUserByNo(
+            @PathVariable Integer user_no) {
+        return userService.getUser(user_no);
+    }
+*/
+    // get
+
 
 }
