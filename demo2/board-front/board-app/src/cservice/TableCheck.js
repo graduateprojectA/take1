95aa7daedb71b7626606496ea83d357c7df3fd09
betty214 <50865204+betty214@users.noreply.github.com>
2021-02-21 20:07:02 +0900

    시간 체크 프론트 2차

--- demo2/board-front/board-app/src/cservice/TableCheck.js
+++ demo2/board-front/board-app/src/cservice/TableCheck.js
@@ -25,33 +25,33 @@ const TableCheck = () => {
               <div className="DayDiv">목</div>
               <div className="DayDiv">금</div>
           </div>
-          <tbody>
-            <tr>
-              <td>1교시</td><td>11</td><td>12</td><td>13</td><td>14</td><td>15</td>
-            </tr>
-            <tr>
-              <td>2교시</td><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td>
-            </tr>
-            <tr>
-              <td>3교시</td><td>31</td><td>32</td><td>33</td><td>34</td><td>35</td>
-            </tr>
-            <tr>
-              <td>4교시</td><td>41</td><td>42</td><td>43</td><td>44</td><td>45</td>
-            </tr>
-            <tr>
-              <td>5교시</td><td>51</td><td>52</td><td>53</td><td>54</td><td>55</td>
-            </tr>
-            <tr>
-              <td>6교시</td><td>Ipsum</td><td>Dolor</td>
-            </tr>
-            <tr>
-              <td>7교시</td><td>Ipsum</td><td>Dolor</td>
-            </tr>
-          </tbody>
+          <div className="TableBody">
+            <div className="TimeWrapDiv">
+              <div className="TimeDiv">1교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
+            </div>
+            <div className="TimeWrapDiv">
+              <div className="TimeDiv">1교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
+            </div>
+            <div className="TimeWrapDiv">
+              <div className="TimeDiv">1교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
+            </div>
+            <div className="TimeWrapDiv">
+              <div className="TimeDiv">1교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
+            </div>
+            <div className="TimeWrapDiv">
+              <div className="TimeDiv">1교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
+            </div>
+            <div className="TimeWrapDiv">
+              <div className="TimeDiv">1교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
+            </div>
+            <div className="TimeWrapDiv">
+              <div className="TimeDiv">1교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>    
-    );
+    </div>
+  );
 };
 
 export default TableCheck;
