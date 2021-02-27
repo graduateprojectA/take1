d129c896a5a9b266aebc4b638bf5a219126d74e1
betty214 <50865204+betty214@users.noreply.github.com>
2021-02-27 20:00:59 +0900

    기본 체크박스 구현

--- demo2/board-front/board-app/src/cservice/EventPractice.js
+++ demo2/board-front/board-app/src/cservice/EventPractice.js
@@ -6,60 +6,27 @@ import Logo from "../cservice/Logo";
 import My from "../cservice/My";
 
 class EventPractice extends Component {
-    state = {
-      checked: false
+  constructor(props) {
+    super(props);
+    this.state = {
+        box1: false,
+        box2: false,
+        box3: false,
     };
-  
-    handleChange = (e) => {
-      const { target: { checked } } = e;
-      this.setState({ checked });
-    };
-  
-    render() {
-      return (
-        <div className="TableCheckDiv">
-            <Logo />
-            <My />
-            <Link to="./classCheck">
-              <button className="NextA">&#10095;</button>
-            </Link>
-            <div className="TableCheckWrapWrapDiv">
-              <div className="TimeTable">
-                <div className="DayWrapDiv">
-                    <div className="DayDiv">월</div>
-                    <div className="DayDiv">화</div>
-                    <div className="DayDiv">수</div>
-                    <div className="DayDiv">목</div>
-                    <div className="DayDiv">금</div>
-                </div>
-                <div className="TableBody">
-                  <div className="TimeWrapDiv">
-                    <div className="TimeDiv">1교시</div><button className="TimeDiv"  value={1} onChange={this.changeTest1Handler}>11</button><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
-                  </div>
-                  <div className="TimeWrapDiv">
-                    <div className="TimeDiv">2교시</div><button className="TimeDiv" onChange={this.changeTest2Handler}>21</button><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
-                  </div>
-                  <div className="TimeWrapDiv">
-                    <div className="TimeDiv">3교시</div><button className="TimeDiv" onClick={this.createTest}>31</button><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
-                  </div>
-                  <div className="TimeWrapDiv">
-                    <div className="TimeDiv">4교시</div><input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
-                  </div>
-                  <div className="TimeWrapDiv">
-                    <div className="TimeDiv">5교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
-                  </div>
-                  <div className="TimeWrapDiv">
-                    <div className="TimeDiv">6교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
-                  </div>
-                  <div className="TimeWrapDiv">
-                    <div className="TimeDiv">7교시</div><div className="TimeDiv">11</div><div className="TimeDiv">12</div><div className="TimeDiv">13</div><div className="TimeDiv">14</div><div className="TimeDiv">15</div>
-                  </div>
-                </div>
-              </div>
-            </div>
+}
+handleChange = (evt) => {
+    const box = evt.target.name;
+    this.setState({ [box]: !this.state.box });
+};
+render() {
+    return (
+        <div>
+            <input type="checkbox" name="box1" onChange={this.handleChange} />
+            <input type="checkbox" name="box2" onChange={this.handleChange} />
+            <input type="checkbox" name="box3" onChange={this.handleChange} />
         </div>
-      );
-    }
+    );
+}
   }
 
 
