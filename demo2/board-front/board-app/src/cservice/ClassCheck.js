c71abd1da087bb8fe5eb68d4f750465527d1beb2
betty214 <50865204+betty214@users.noreply.github.com>
2021-03-18 23:47:02 +0900

    dropdown 삽입

--- demo2/board-front/board-app/src/cservice/ClassCheck.js
+++ demo2/board-front/board-app/src/cservice/ClassCheck.js
@@ -149,27 +149,16 @@ class ClassCheck extends Component {
                 displayValue="class_no"
                 showCheckbox={true}
             />
-            <div class="dropdown">
-              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown button
-                </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </div>
-            <div class="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
             <button onClick={() => console.log(this.state.newArr)} />
             <button onClick={()=>console.log(this.state.filArr)} />
-            <div className="ClassDiv">
-                <ul>
+            <div className="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown button
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                 {
                   this.state.newArr.map(c =>
-                    <li><input type="checkbox" value={c.class_no} onChange={this.handleChange}/>{c.class_no}</li>)
+                    <li><input type="checkbox" class="dropdown-item" value={c.class_no} onChange={this.handleChange}/>{c.class_no}</li>)
                 }
               </ul>
             </div>
