d16c78d67e1f1f10c632fbbe79372f27834230a4
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2021-03-23 12:32:41 +0900

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -68,8 +68,15 @@ class_no int auto_increment primary key,
 class_name varchar(300),
 course_id int, 	#학수번호
 professor_name varchar(100),
-class_division int, 	# 분반
-class_time int 	#해당학기의 시간
+class_division int, 	#분반
+class_time int, 	#해당학기의 시간
+exam_per int,	#중간, 기말
+quiz_per int, 	#퀴즈
+presentation_per int, 	#발표
+project_per int,	#프로젝트
+assignment_per int,	#과제물
+attendance_per int, 	#출석
+extra_per int	#기타
 );
 
 create table User_check_field(
@@ -113,6 +120,17 @@ class_pre boolean,
 class_next boolean,
 foreign key(user_no) references User(user_no) on update cascade on delete cascade,
 foreign key(class_no) references Class(class_no) on update cascade on delete cascade
+);
+
+# 사용자가 선택한 가중치값
+create table User_preperence(
+user_no int primary key,
+exam_pre int,	#중간, 기말
+quiz_pre int, 	#퀴즈
+presentation_pre int, 	#발표
+project_pre int,	#프로젝트
+assingment_pre int,	#과제물
+attendance_pre int		#출석
 );
 
 #사용자가 시간 선택
