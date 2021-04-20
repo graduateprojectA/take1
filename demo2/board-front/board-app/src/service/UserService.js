caabccc34e8a21205cc563ed34b1135b5bf5eb1b
betty214 <50865204+betty214@users.noreply.github.com>
2021-04-20 23:28:55 +0900

    프론트 userservice 주소 호스팅용으로 변경

--- demo2/board-front/board-app/src/service/UserService.js
+++ demo2/board-front/board-app/src/service/UserService.js
@@ -1,43 +1,43 @@
 import axios from 'axios';
 class UserService {
     createUser(user) {
-        return axios.post("http://localhost:8080/api/user", user);
+        return axios.post("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/user", user);
     }
     loginUser(user) {
-        return axios.post("http://localhost:8080/api/login", user);
+        return axios.post("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/login", user);
     }
     TimeUser(time) {
-        return axios.post("http://localhost:8080/api/tableCheck", time);
+        return axios.post("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/tableCheck", time);
     }
     login() {
-        return axios.get("http://localhost:8080/api/login2");
+        return axios.get("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/login2");
     }
     courseUser(user_no) {
-        return axios.post("http://localhost:8080/api/courseUser",user_no);
+        return axios.post("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/courseUser",user_no);
     }
     SendClassUser(user_course) {
-        return axios.post("http://localhost:8080/api/courseCheck", user_course);
+        return axios.post("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/courseCheck", user_course);
     }
     course2User() {
-        return axios.get("http://localhost:8080/api/course");
+        return axios.get("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/course");
     }
     classUser(user_no) {
-        return axios.post("http://localhost:8080/api/classUser", user_no);
+        return axios.post("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/classUser", user_no);
     }
     class2User() {
-        return axios.get("http://localhost:8080/api/class");
+        return axios.get("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/class");
     }
     CheckClassUser(user_class) {
-        return axios.post("http://localhost:8080/api/classCheck", user_class);
+        return axios.post("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/classCheck", user_class);
     }
     postUser(user_no){
         return axios({method :'post',
-        url:'http://localhost:8080/api/courseUser', 
+        url:'http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/courseUser', 
         headers :{'Content-Type': 'application/json' }, 
         data: user_no});
     }
     SendPre(pre) {
-        return axios.post("http://localhost:8080/api/preferenceUser", pre);
+        return axios.post("http://ec2-3-35-209-240.ap-northeast-2.compute.amazonaws.com:8080/api/preferenceUser", pre);
     }
 }
 
