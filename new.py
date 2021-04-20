2d3bc3028eddb7dba3e552080defa3cd0142ddd8
betty214 <50865204+betty214@users.noreply.github.com>
2021-04-20 20:34:57 +0900

    db host 위치 변경

--- new.py
+++ new.py
@@ -4,7 +4,7 @@ import csv
 import pymysql
 
 # Majors
-conn = pymysql.connect(host='127.0.0.1', user='root', password='wlsgml214', db='graduate', charset='utf8')
+conn = pymysql.connect(host='database-1.c0vf4uulw0ya.ap-northeast-2.rds.amazonaws.com', user='root', password='12345678', db='graduate', charset='utf8')
 curs = conn.cursor()
 conn.commit()
 f = open('Majors.csv','r',encoding = 'utf-8-sig')
@@ -12,7 +12,7 @@ csvReader = csv.reader(f)
 for row in csvReader:    
     major_name = row[0]
     major_name = "\"" + major_name + "\""
-    sql = "insert into graduate.majors (major_name) values (%s)" %(major_name)
+    sql = "insert into graduate.Majors (major_name) values (%s)" %(major_name)
     curs.execute(sql)
 conn.commit()
 f.close() 
