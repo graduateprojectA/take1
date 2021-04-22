4551b47022344067d744712c75b8f145ba46d8e3
김서영 <ksy990628@gmail.com>
2021-04-23 02:55:52 +0900

    가중치값 UI modify

--- demo2/board-front/board-app/src/cservice/PreCheck.js
+++ demo2/board-front/board-app/src/cservice/PreCheck.js
@@ -7,19 +7,18 @@ import My from "./My";
 import backgroundImage2 from "../components/image/backgroundImage2.png";
 import  CheckBox  from './CheckBox';
 
-// const ClassCheckWrapWrapDiv = styled.div`
-//     position: absolute;
-//     top: 120px;
-//     left: 50%;
-//     margin-left: -370px;
-//     border: solid 1px;
-//     height: 585px;
-//     width: 740px;
-//     background-color: #fff5ee;
-//     opacity: 0.8;
-//     border: solid 5px #00462A;
-//     border-radius: 10px;
-// `;
+const ReDiv = styled.div`
+margin-left: 60%;
+`
+
+const RESET = styled.button`
+margin-top: 2%;
+margin-left:3%;
+font-size: 1.3em;
+border:none;
+border-radius: 10%;
+background-color:yellow;
+`
 
 class PhoneInfo extends Component {
   constructor(props) {
@@ -82,18 +81,49 @@ completeCheck = (event) => {
             중요하게 생각하시는 항목을 순서대로 선택 해주세요.
             <br/> 우측 흰색 화살표를 눌러, 다음 페이지로 이동하세요.</h4>
             <br/>
-            <button onClick={() => console.log(this.state.exam_pre)} />
-            <button onClick={this.reset}>reset</button>
-            <tr className="TimeDiv">    
-            <input type="checkbox" name="exam_pre" id="order" onChange={this.handleChange}/>시험
-            <input type="checkbox" name="quiz_pre" onChange={this.handleChange}/>퀴즈
-            <input type="checkbox" name="presentation_pre" onChange={this.handleChange}/>발표
-            <input type="checkbox" name="project_pre" onChange={this.handleChange}/>프로젝트
-            <input type="checkbox" name="assignment_pre" onChange={this.handleChange}/>과제
-            <input type="checkbox" name="attendance_pre" onChange={this.handleChange}/>출석
-            <br/>
-          </tr>
-            {this.state.exam_pre} {this.state.quiz_pre} {this.state.presentation_pre} {this.state.project_pre} {this.state.assignment_pre} {this.state.attendance_pre}
+            <ReDiv>
+            <RESET onClick={() => console.log(this.state.exam_pre)}>로딩하기</RESET>
+            <RESET onClick={this.reset}>다시 설정하기</RESET>
+            {/* <button onClick={this.reset}>다시 설정하기</button> */}
+            </ReDiv>
+            <table className="WeightTable">
+              <tr classname="TimeDiv">
+              {/* <th className="DayDiv"><input type="checkbox" name="exam_pre" id="test" className="testDiv" onChange={this.handleChange}/><label for="test">시험</label></th> */}
+              <th className="WeightLabelDiv">시험</th>
+              <th className="DayDiv"><input type="checkbox" name="exam_pre" className="WeightDiv" onChange={this.handleChange}/></th>
+              <th className="WeightLabelDiv">{this.state.exam_pre}순위</th>
+              </tr>
+
+              <tr classname="TimeDiv">
+              <th className="WeightLabelDiv">퀴즈</th>
+              <th className="DayDiv"><input type="checkbox" name="quiz_pre" className="WeightDiv" onChange={this.handleChange}/></th>
+              <th className="WeightLabelDiv">{this.state.quiz_pre}순위</th>
+              </tr>
+
+              <tr classname="TimeDiv">
+              <th className="WeightLabelDiv">발표</th>
+              <th className="DayDiv"><input type="checkbox" name="presentation_pre" className="WeightDiv" onChange={this.handleChange}/></th>
+              <th className="WeightLabelDiv">{this.state.presentation_pre}순위</th>
+              </tr>
+
+              <tr classname="TimeDiv">
+              <th className="WeightLabelDiv">프로젝트</th>
+              <th className="DayDiv"><input type="checkbox" name="project_pre" className="WeightDiv" onChange={this.handleChange}/></th>
+              <th className="WeightLabelDiv">{this.state.project_pre}순위</th>
+              </tr>
+
+              <tr classname="TimeDiv">
+              <th className="WeightLabelDiv">과제</th>
+              <th className="DayDiv"><input type="checkbox" name="assignment_pre" className="WeightDiv" onChange={this.handleChange}/></th>
+              <th className="WeightLabelDiv">{this.state.assignment_pre}순위</th>
+              </tr>
+
+              <tr classname="TimeDiv">
+              <th className="WeightLabelDiv">출석</th>
+              <th className="DayDiv"><input type="checkbox" name="attendance_pre" className="WeightDiv" onChange={this.handleChange}/></th>
+              <th className="WeightLabelDiv">{this.state.attendance_pre}순위</th>
+              </tr>
+              </table>
           </div>
         </div>
     );
